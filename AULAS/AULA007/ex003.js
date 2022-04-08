ea2ed46e675b3c99bd091f70e343e698b46cb3a9
joaowictorzanin <103003839+joaowictorzanin@users.noreply.github.com>
2022-04-08 13:45:56 -0300

    Create ex003.js

---
+++ AULAS/AULA007/ex003.js
@@ -0,0 +1,9 @@
+var vel = 80
+
+console.log(`A velocidade do seu carro é ${vel}Km/h...`)
+
+if (vel > 60) { //condição simples
+    console.log('Você foi multado!!!')    
+}
+
+console.log('Dirija sempre com o cinto de segurança')
