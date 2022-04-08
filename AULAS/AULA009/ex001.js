cb8a15e9eea1272aa9b1b8c414b3a1717f5fe33b
joaowictorzanin <103003839+joaowictorzanin@users.noreply.github.com>
2022-04-08 14:05:59 -0300

    Create ex001.js

---
+++ AULAS/AULA009/ex001.js
@@ -0,0 +1,15 @@
+var c = 1
+
+// Primeiro ele executa depois ele testa
+do {
+    console.log(`Teste ${c}`)
+    c ++
+} while (c <= 6);
+
+/*
+Primeiro teste depois executa
+while (c <= 5) {
+    console.log(`Teste ${c}`)
+    c ++
+}
+*/
